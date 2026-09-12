19e80a76e257861e0cf12e3f8493c97689b8a223
okuda1506 <okuda1506@gmail.com>
2026-09-12 14:44:14 +0900

    Initial Commit

---
+++ BeautyArchive/BeautyArchiveApp.swift
@@ -0,0 +1,17 @@
+//
+//  BeautyArchiveApp.swift
+//  BeautyArchive
+//
+//  Created by 奥田拓也 on 2026/09/12.
+//
+
+import SwiftUI
+
+@main
+struct BeautyArchiveApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ BeautyArchiveTests/BeautyArchiveTests.swift
@@ -0,0 +1,19 @@
+//
+//  BeautyArchiveTests.swift
+//  BeautyArchiveTests
+//
+//  Created by 奥田拓也 on 2026/09/12.
+//
+
+import Testing
+@testable import BeautyArchive
+
+struct BeautyArchiveTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+        // Swift Testing Documentation
+        // https://developer.apple.com/documentation/testing
+    }
+
+}
